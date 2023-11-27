db55f3d32a9c239debc116b4d00d5b88c8d774d9
Azarito20 <131048252+Azarito20@users.noreply.github.com>
2023-11-26 22:05:38 -0400

    Add files via upload

--- Proyecto2EDD/src/proyecto2edd/main.java
+++ Proyecto2EDD/src/proyecto2edd/main.java
@@ -5,7 +5,8 @@ package proyecto2edd;
 public class main {
 
     public static void main(String[] args) {
-        
+        BinaryHeap monticulo = new BinaryHeap();
+        monticulo.insertNodo(34);
+        monticulo.print();
     }
-    
 }
