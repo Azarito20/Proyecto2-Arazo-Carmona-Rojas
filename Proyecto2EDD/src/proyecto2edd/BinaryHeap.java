95be61b20096e5e19def3495463cddaebb01ea11
Azarito20 <131048252+Azarito20@users.noreply.github.com>
2023-11-26 20:18:28 -0400

    Add files via upload

--- Proyecto2EDD/src/proyecto2edd/BinaryHeap.java
+++ Proyecto2EDD/src/proyecto2edd/BinaryHeap.java
@@ -38,12 +38,46 @@ public class BinaryHeap {
         nodo.setIndex(nodo.getIndex() + getSize());
         size++;
         if ( getSize() -1 != 0) {
-            rearrange(nodo, getSize() -1);
+            rearrangeUp(getSize() -1);
         }
     }
     
-    public void rearrange(Object element, int index) {
-        NodoArray nodo = new NodoArray(element);
-        
+    public void rearrangeUp(int index) {
+        NodoArray pointer = documents[index];
+        int temp = (index-1)/2;
+        while (documents[index].getIndex() < documents[temp].getIndex()) {
+            documents[index] = documents[temp];
+            documents[temp] = pointer;
+        }
+    }
+    
+    public void rearrangeDown (int index) {
+        NodoArray pointer = documents[index];
+        int temp1 = (index*2)+1;
+        int temp2 = (index*2)+2;
+        if (documents[index].getIndex() > documents[temp1].getIndex()) {
+            documents[index] = documents[temp1];
+            documents[temp1] = pointer;
+        } else if (documents[index].getIndex() > documents[temp2].getIndex()) {
+            documents[index] = documents[temp2];
+            documents[temp2] = pointer;
+        }
+    }
+    
+    public NodoArray deleteRoot() {
+        NodoArray nodo = documents [0];
+        documents [0] = documents [size -1];
+        size--;
+        rearrangeDown(0);
+        return nodo;
+    }
+    
+    public void print() {
+        int i = 0;
+        NodoArray pointer = documents[i];
+        while (i < getSize()) {
+            System.out.println("[" + documents[i].getIndex() + "]");
+            i++;
+        }
     }
 }
--- Proyecto2EDD/src/proyecto2edd/main.java
+++ Proyecto2EDD/src/proyecto2edd/main.java
@@ -4,8 +4,15 @@ package proyecto2edd;
 
 public class main {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main() {
+        BinaryHeap monticulo = new BinaryHeap();
+        monticulo.insertNodo(50);
+        monticulo.insertNodo(51);
+        monticulo.insertNodo(52);
+        monticulo.insertNodo(53);
+        monticulo.insertNodo(54);
+        monticulo.insertNodo(55);
+        monticulo.insertNodo(56);
+        monticulo.print();
     }
-    
 }
